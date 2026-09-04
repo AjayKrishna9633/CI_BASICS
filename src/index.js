5c7f1aeded69a3089dc49196caaa788e7e34a4dd
Ajaykrihnaajay9633 <Krishnaajay802@gmail.com>
2026-09-04 16:07:34 +0530

    this for to fail the test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ function calculateDiscount(price, discountPercent) {
   if (discountPercent < 0 || discountPercent > 100) {
     throw new Error("Discount percent must be between 0 and 100");
   }
-  return price - (price * (discountPercent / 100));
+  // 💥 INTENTIONAL BUG: Adding discount instead of subtracting!
+  return price + (price * (discountPercent / 100));
 }
 
 module.exports = { sanitizeUsername, calculateDiscount };
